aabbd735ab86315adf32fadc7e2009918c407feb
AlejandroB10 <alebordon111@gmail.com>
2024-03-10 22:11:27 +0100

    some changes more in deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://AlejandroB10.github.io'
+  base: 'https://AlejandroB10.github.io'
 });
